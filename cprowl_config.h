3d4b6f18c6be9e1079475bd033a59414572a7aae
Mark Clayton <clayton256@gmail.com>
2017-12-31 20:00:36 -0500

    initial commit
    save me from having to use waf

---
+++ cprowl_config.h
@@ -0,0 +1,8 @@
+/* Configuration header created by Waf - do not edit */
+#ifndef _CPROWL_CONFIG_H_WAF
+#define _CPROWL_CONFIG_H_WAF
+
+#define HAVE_LIBCURL 1
+#define CPROWL_VERSION "0.5.1"
+#define CPROWL_NAME "cprowl"
+#endif /* _CPROWL_CONFIG_H_WAF */
